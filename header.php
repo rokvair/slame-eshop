b9ad8dfb393f25681b2fac4adb8801f965904bbf
Rugilė Jovaišaitė <115405668+gilerugile@users.noreply.github.com>
2024-12-15 23:06:46 +0200

    Pridėtas minimalus headeris

--- header.php
+++ header.php
@@ -4,6 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <header>
+        <nav>
+            <div class="logo">Slime E-Shop</div>
+            <ul class="nav-links">
+                <li><a href="index.php">Namai</a></li>
+                <li><a href="all_items.php">Prekės</a></li>
+                <li><a href="login.html" class="btn-login">Login</a></li>
+                <li><a href="signup.html" class="btn-signup">Sign Up</a></li>
+                </li>
+            </ul>
+        </nav>
+    </header>
+<body>
+</html>
